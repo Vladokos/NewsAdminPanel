42346ec7aebb9f9f5cdb279a1567bb9569ce2225
Vlad <66181660+Vladokos@users.noreply.github.com>
2021-08-24 16:03:46 +0400

    master

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.post("/text", jsonParser, (req, res) => {
     text: text,
   };
   fs.writeFile("textUsers.json", JSON.stringify(document), (err) => {
-    if (err) throw err;
+    if (err) throw err
   });
 });
 
