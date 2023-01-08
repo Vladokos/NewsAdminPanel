68e9b06ab12c977c93a1754d860e9f0d47ef008e
Vlad <66181660+Vladokos@users.noreply.github.com>
2023-01-09 00:59:54 +0400

    interface

---
+++ client/src/interface/article.ts
@@ -0,0 +1,14 @@
+export interface ArticleInterface{
+    title: string,
+    category: string,
+    text: string
+}
+
+export interface Articles{
+    _id:string,
+    category: string,
+    title: string,
+    text: string,
+    image:string
+}
+
