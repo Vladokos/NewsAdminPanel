47535681672cb0bd91dd4b0fb693f598bbe46b3d
Vlad <66181660+Vladokos@users.noreply.github.com>
2023-01-09 00:59:40 +0400

    moved to tsx

---
+++ client/src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Panel from "./panel";
+
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Panel />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
